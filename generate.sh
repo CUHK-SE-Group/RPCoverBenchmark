b0249c1454be2ca682eb9038cde24f228d07f92d
lincyawer <53161583+Lincyaw@users.noreply.github.com>
2023-06-26 14:48:05 +0800

    add protos

--- generate.sh
+++ generate.sh
@@ -2,12 +2,19 @@
 
 PROTOC_GEN_GO=$(which protoc-gen-go)
 PROTOC_GEN_GO_GRPC=$(which protoc-gen-go-grpc)
+PROTOC_GEN_PY=$(which grpc_python_plugin)
+PROTOC_GEN_PY_GRPC=$(which grpc_python_plugin)
 PROTO_DIR=protos
-GO_OUT_DIRS=(Go_A Go_B Go_C Cpp_B Cpp_C Java_A Java_B Ts_B Ts_C)
+GO_OUT_DIRS=(Go_A Go_B Go_C Cpp_A Cpp_B Cpp_C Java_A Java_B Java_C Ts_A Ts_B Ts_C Python_A Python_B Python_C )
 
 for dir in "${GO_OUT_DIRS[@]}"; do
   if [ ! -d "$dir" ]; then
     mkdir "$dir"
   fi
   protoc --go_out=Go_A --go-grpc_out=Go_A --plugin=protoc-gen-go=$PROTOC_GEN_GO --plugin=protoc-gen-go-grpc=$PROTOC_GEN_GO_GRPC $PROTO_DIR/$dir.proto
+  protoc --go_out=Go_B --go-grpc_out=Go_B --plugin=protoc-gen-go=$PROTOC_GEN_GO --plugin=protoc-gen-go-grpc=$PROTOC_GEN_GO_GRPC $PROTO_DIR/$dir.proto
+  protoc --go_out=Go_C --go-grpc_out=Go_C --plugin=protoc-gen-go=$PROTOC_GEN_GO --plugin=protoc-gen-go-grpc=$PROTOC_GEN_GO_GRPC $PROTO_DIR/$dir.proto
+  protoc --python_out=Python_A --python-grpc_out=Python_A --plugin=protoc-gen-python-grpc=$PROTOC_GEN_PY_GRPC $PROTO_DIR/$dir.proto
+  protoc --python_out=Python_B --python-grpc_out=Python_B --plugin=protoc-gen-python-grpc=$PROTOC_GEN_PY_GRPC $PROTO_DIR/$dir.proto
+  protoc --python_out=Python_C --python-grpc_out=Python_C --plugin=protoc-gen-python-grpc=$PROTOC_GEN_PY_GRPC $PROTO_DIR/$dir.proto
 done
