e370e93384bfd5296208818473ddf413fe704e5a
134ARG <xen134@outlook.com>
2023-06-27 18:24:48 +0800

    fix missing grpc lib when linking

--- Cpp_A/src/server.cc
+++ Cpp_A/src/server.cc
@@ -4,4 +4,12 @@
                                  const ::protos::CommonMessage *request,
                                  ::protos::CommonMessage *response) {}
 
+::grpc::Status CppAImpl::Cpp_A_2(::grpc::ServerContext *context,
+                                 const ::protos::CommonMessage *request,
+                                 ::protos::CommonMessage *response) {}
+
+::grpc::Status CppAImpl::Cpp_A_3(::grpc::ServerContext *context,
+                                 const ::protos::CommonMessage *request,
+                                 ::protos::CommonMessage *response) {}
+
 int main(void) { return 0; }
