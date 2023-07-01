9e9ad2d53ce089a3d51403a88fb05bd859768bf8
134ARG <xen134@outlook.com>
2023-07-01 13:25:41 +0800

    update Cpp_A

--- Cpp_A/src/server.h
+++ Cpp_A/src/server.h
@@ -1,11 +1,14 @@
 #include "protos/Cpp_A.grpc.pb.h"
 #include "protos/Cpp_A.pb.h"
+#include <cstdint>
 #include <grpc/grpc.h>
 #include <grpcpp/server.h>
+#include <stdint.h>
 
 using namespace protos;
 
 class CppAImpl final : public ::Cpp_A::Service {
+public:
   ::grpc::Status Cpp_A_1(::grpc::ServerContext *context,
                          const ::protos::CommonMessage *request,
                          ::protos::CommonMessage *response);
@@ -15,4 +18,9 @@ class CppAImpl final : public ::Cpp_A::Service {
   ::grpc::Status Cpp_A_3(::grpc::ServerContext *context,
                          const ::protos::CommonMessage *request,
                          ::protos::CommonMessage *response);
+
+private:
+  uint32_t cnt = 0;
+
+  uint32_t rand();
 };
