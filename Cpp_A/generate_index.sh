c6f15d21a3a111d19229bd92eef2d207423330d3
134ARG <xen134@outlook.com>
2023-07-10 23:06:02 +0800

    add index generation for cpp cases

---
+++ Cpp_A/generate_index.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PREV_DIR=$(pwd)
+CASE_DIR=$(readlink -f $(dirname "$0"))
+ROOT_DIR=$CASE_DIR/..
+SCIP_CLANG_DIR=$ROOT_DIR/third_party/scip-clang-0.2.3
+SCIP_CLANG_BIN=$SCIP_CLANG_DIR/bazel-bin/indexer/scip-clang
+
+CC_PATH=$CASE_DIR/build/compile_commands.json
+
+cd $CASE_DIR
+
+if [ -d "build" ]; then
+    rm -rf ./build
+fi
+
+mkdir build && cd build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
+
+cd $SCIP_CLANG_DIR
+bazel build //indexer:scip-clang --config=release
+
+cd $ROOT_DIR
+$SCIP_CLANG_BIN --compdb-path=$CC_PATH --index-output-path=$CASE_DIR/index.scip
+
+cd $PREV_DIR
