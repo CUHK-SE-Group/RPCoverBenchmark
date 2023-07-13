a8923830bf4230109b1f5372bb9b41d5452c0b42
134ARG <xen134@outlook.com>
2023-07-13 17:42:45 +0800

    fix wrong python venv init cmd

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg && mv bazel-archive-keyring.gpg /usr/share/keyrings
 # echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 # apt update && apt install wget build-essential ninja-build cmake golang gcc clang libgrpc++-dev libgrpc-dev protobuf-compiler-grpc protobuf-compiler python3 python-is-python3 python3-pip pipx openjdk-11-jdk bazel bazel-5.3.0 bazel-6.1.2 npm -y
-python python -m venv venv
+python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 npm install && npm install -g @sourcegraph/scip-python && npm install -g @sourcegraph/scip-typescript
