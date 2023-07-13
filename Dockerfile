48cc51e0f0e75014b078e765b12c294c47383c3c
134ARG <xen134@outlook.com>
2023-07-13 17:27:17 +0800

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM debian:unstable
+
+RUN rm /etc/apt/sources.list.d/debian.sources && echo "deb http://mirrors.ustc.edu.cn/debian/ sid main contrib" >/etc/apt/sources.list && apt update && apt install apt-transport-https curl gnupg -y && curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg && mv bazel-archive-keyring.gpg /usr/share/keyrings && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+RUN apt update && apt install wget build-essential ninja-build cmake golang gcc clang libgrpc++-dev libgrpc-dev protobuf-compiler-grpc protobuf-compiler python3 python-is-python3 python3-pip pipx openjdk-11-jdk bazel bazel-5.3.0 bazel-6.1.2 npm openjdk-17-jdk -y
+
 COPY third_party /opt/third_party/
 COPY . /opt/RPCoverBenchmark
 WORKDIR /opt/RPCoverBenchmark
+
 RUN bash -c "./install.sh"
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm /etc/apt/sources.list.d/debian.sources
-echo "deb http://mirrors.ustc.edu.cn/debian/ sid main contrib" >/etc/apt/sources.list
-apt update && apt install apt-transport-https curl gnupg -y
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg && mv bazel-archive-keyring.gpg /usr/share/keyrings
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update && apt install wget build-essential ninja-build cmake golang gcc clang libgrpc++-dev libgrpc-dev protobuf-compiler-grpc protobuf-compiler python3 python-is-python3 python3-pip pipx openjdk-11-jdk bazel bazel-5.3.0 bazel-6.1.2 npm -y
-pipx install psrecord
-pipx install click
-pipx install json
+# rm /etc/apt/sources.list.d/debian.sources
+# echo "deb http://mirrors.ustc.edu.cn/debian/ sid main contrib" >/etc/apt/sources.list
+# apt update && apt install apt-transport-https curl gnupg -y
+# curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg && mv bazel-archive-keyring.gpg /usr/share/keyrings
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+# apt update && apt install wget build-essential ninja-build cmake golang gcc clang libgrpc++-dev libgrpc-dev protobuf-compiler-grpc protobuf-compiler python3 python-is-python3 python3-pip pipx openjdk-11-jdk bazel bazel-5.3.0 bazel-6.1.2 npm -y
+python python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 npm install && npm install -g @sourcegraph/scip-python && npm install -g @sourcegraph/scip-typescript
